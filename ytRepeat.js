0f77f552bd0e7b36c9ac83ad25dd9ebf36e17354
Witold Łojek <witoldlojek@MacBook-Pro-WitoldMac.local>
2018-07-09 02:15:47 +0200

    Removed MutationObserver and added eventListener

--- ytRepeat.js
+++ ytRepeat.js
@@ -1,27 +1,7 @@
-function YtRepeat(endScreen, playBtn) {
-  this.endScreen = endScreen;
-  this.playBtn = playBtn;
-
-  this.repeat = function() {
-    let endScreenElement = $(this.endScreen);
-    let playBtnElement = $(this.playBtn);
-
-    if (endScreenElement.css('display') !== 'none') {
-      playBtnElement.click();
-    }
-  };
-}
-
 $(document).ready(function() {
-  const endScreenCss = '.videowall-endscreen';
-  const playBtnCss = '.ytp-play-button';
-  var ytRepeatObj = new YtRepeat(endScreenCss, playBtnCss);
-
-  var observer = new MutationObserver(function(mutations) {
-    mutations.forEach(function(mutationRecord) {
-      ytRepeatObj.repeat();
-    });
+  document.querySelector('video').addEventListener('ended', function() {
+    let elements = document.querySelectorAll('.ytp-play-button');
+    let playBtn = elements[0];
+    playBtn.click();
   });
-  var elements = document.querySelectorAll(playBtnCss);
-  observer.observe(elements[0], { attributes: true });
 });
